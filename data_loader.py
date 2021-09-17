b15f815cd4daf8a258fbc41a0274aef761c1be2a
praneethtkonda <praneethtkonda@gmail.com>
2021-09-17 18:12:13 +0530

    Added create_index code to avoid duplicates while insertion

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,8 @@
 import json
 import pprint
-
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 file_name = './dataset.txt'
-url_map = set()
-
 
 def transformer(data):
     """
@@ -15,14 +12,10 @@ def transformer(data):
     3. zipcode = outcode + postcode
     4. _id.$oid => id
     """
-    if data['URL'] in url_map:
-        #In Case of duplicate
-        return None
-    url_map.add(data['URL'])
     trans_obj = {}
-    trans_obj['address'] = data['address']
-    trans_obj['address_2'] = data['address line 2']
-    trans_obj['name'] = data['name']
+    trans_obj['address'] = str(data['address'])
+    trans_obj['address_2'] = str(data['address line 2'])
+    trans_obj['name'] = str(data['name'])
     trans_obj['zipcode'] = data['outcode'] + data['postcode']
     trans_obj['rating'] = data['rating']
     trans_obj['type_of_food'] = data['type_of_food']
@@ -33,17 +26,23 @@ if __name__=="__main__":
         client = MongoClient("mongodb://root:example@localhost:27017/")
         db = client['bds']
         restaurants = db['restaurants']
+        # Creating index on name and zipcode
+        res = restaurants.create_index([('name', 1), ('zipcode', 1)], unique = True)
+        print(f"Created Index: {res}")
 
-        final_list = []
         file = open(file_name, "r")
         restaurants_list = file.readlines()
 
         for restaurant in restaurants_list:
             obj = json.loads(restaurant)
             trans_obj = transformer(obj)
-            if trans_obj:
-                print(f"Inserting record {trans_obj['name']}")
+            print(f"Inserting record {trans_obj['name']}")
+            try:
                 restaurants.insert_one(trans_obj)
+            except errors.DuplicateKeyError:
+                print(f"Duplicate key found for {trans_obj['name']}")
+            finally:
+                continue
         # pprint.pprint(restaurants.find_one())
 
     except Exception as e:
